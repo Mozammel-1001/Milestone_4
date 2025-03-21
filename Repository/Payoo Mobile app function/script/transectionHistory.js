8b3ecee5f0aef94ac8383095e2a1696c4ccf8870
MD MOZAMMEL HOSSAIN FAZLE RABBI <mozammelhossain.eee.buet@gmail.com>
2025-03-21 21:02:49 +0600

    After bonus module

--- Repository/Payoo Mobile app function/script/transectionHistory.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("Transaction-box").addEventListener("click", function () {
-    handleToggle("addmoney", "none")
-    handleToggle("cashout", "none")
-    handleToggle("transection-history", "block")
-    
-
-    
-})
